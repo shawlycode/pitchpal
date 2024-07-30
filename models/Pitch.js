61244601266e7d86e95ac32c93364eb3a171b1f4
shawlycode <kutinaorlando@gmail.com>
2024-07-30 01:49:42 +0000

    added comments to pitch model

--- models/Pitch.js
+++ models/Pitch.js
@@ -9,20 +9,20 @@ const pitchSchema = new Schema({
   recordVideo: { type: String }, // record 1 minute video of founder(s)
   companyName: { type: String },
   role: { type: string, enum: ['Founder', 'CTO', 'CEO, Developer', 'Sales and Marketing'] },
-  description: { type: String, required: true },
-  companyUrl: { type: String },
-  demoUrl: { type: String },
-  recordDemo: { type: String },
-  slidesUrl: { type: String },
+  description: { type: String, required: true }, // about your company
+  companyUrl: { type: String },//company website if any
+  demoUrl: { type: String }, //upload pitch demo 
+  recordDemo: { type: String }, //record pitch on screen using the platform
+  slidesUrl: { type: String }, // pitch slides eg. powerPoint
   fundingGoal: { type: Number, required: true },
   fundingAmount: { type: Number, default: 0.00 },
   status: { type: String, enum: ['Open', 'Pending Approval', 'Approved', 'In Progress', 'Closed'], default: 'Closed' },
-  productUrl: { type: String },
-  productDetails: { type: String },
-  ideation: { type: String }, // why the idea
+  productUrl: { type: String }, //link to pitch product if already in the market
+  productDetails: { type: String }, //what the product does
+  ideation: { type: String }, // why the idea/product?
   competitors: { type: String },
-  forcastAmount: { type: Number, default: 0.00 }, // how will you make  money?
-  companyRegDocs: { type: String }
+  forcastAmount: { type: Number, default: 0.00 }, // how will you make  money/ how much have you made so far?
+  companyRegDocs: { type: String } // company legal documents
 
 },
 
