4d227ece28fd227bcc0c8d7bb2488ba6c1d955e4
shawlycode <kutinaorlando@gmail.com>
2024-08-03 22:40:02 +0000

    updated

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ pitchpal.use(cors({ credentials: true, origin: '*' }));
 //listening to port
 const PORT = process.env.PORT || 8090
 pitchpal.use('api/users', userRouter);
-pitchpal.use('api/users', pitchRouter);
+pitchpal.use('/users', pitchRouter);
 
 expressOasGenerator.handleRequests();
 pitchpal.use((req, res) => res.redirect('/api-docs/'));
